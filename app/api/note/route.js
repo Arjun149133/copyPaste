29c73ca283c29707ed070977834ae7feed57e0f4
Arjun <arjun149133@gmail.com>
2024-06-16 22:24:27 +0530

    initial connect

---
+++ app/api/note/route.js
@@ -0,0 +1,22 @@
+import { connectToDatabase } from "@/lib/database/mongoose";
+import Note from "@/lib/database/models/note";
+
+export const GET = async (req, res) => {
+  try {
+    await connectToDatabase();
+
+    const notes = await Note.find({});
+    return new Response(JSON.stringify(notes), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        msg: error.message,
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
